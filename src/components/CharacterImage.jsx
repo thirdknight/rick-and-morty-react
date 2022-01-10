83ae46dcbc57d83e20c81946b598c7908158b864
Edinson <edinson.ningles@gmail.com>
2022-01-10 11:39:37 +0100

    Add styles to CharacterImage

--- src/components/CharacterImage.jsx
+++ src/components/CharacterImage.jsx
@@ -1,11 +1,8 @@
-// import styles from "./CharacterImage.module.css";
+import styles from "./CharacterImage.module.css";
 
 export default function CharacterImage({ image, name }) {
   return (
-    <>
-      {/* <div className={styles.characterImageArea}> */}
-        <img className="character-image" src={image} alt={name} />
-      {/* </div> */}
-    </>
+      <img className={styles.characterImage} src={image} alt={name} />
+    
   )
 }
